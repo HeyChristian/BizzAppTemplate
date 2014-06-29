6810e22f190c1939a5f451b8d6bbd789fdfdf212
Christian Vazquez <colapsov@gmail.com>
2014-06-29 18:56:03 -0500

    Initial Commit

---
+++ BizzAppTemplate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BizzAppTemplate
+//
+//  Created by Christian Vazquez on 6/29/14.
+//  Copyright (c) 2014 Christian Vazquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BizzAppTemplateTests/BizzAppTemplateTests.m
@@ -0,0 +1,34 @@
+//
+//  BizzAppTemplateTests.m
+//  BizzAppTemplateTests
+//
+//  Created by Christian Vazquez on 6/29/14.
+//  Copyright (c) 2014 Christian Vazquez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BizzAppTemplateTests : XCTestCase
+
+@end
+
+@implementation BizzAppTemplateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
